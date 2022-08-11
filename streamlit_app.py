0ee7e8a7b70e5a7f15bea6b96c687360d3b77cb8
rahammo <97483934+rahammo@users.noreply.github.com>
2022-08-11 15:38:12 -0500

    Update streamlit_app.py
    removed json format of fruit content

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # Stores fruitvice fruit content in a variable
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
