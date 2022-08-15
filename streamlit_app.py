6d9aa58c6340f9bd78d774e620d23dd3b88f4710
rahammo <97483934+rahammo@users.noreply.github.com>
2022-08-15 13:18:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,6 +13,7 @@ streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
 
 streamlit.header('🍓🍌 Build Your Smoothie 🍍🍎')
+# import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,7 +29,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+# import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Stores fruitvice fruit content in a variable
@@ -33,8 +37,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display fruit content in a dataframe (aka table format)
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 # Connecting to Snowflake instance
-import snowflake.connector
+# import snowflake.connector
 
 # Using configs set in the streamlit app settings to conenct to acct.
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -48,15 +54,7 @@ streamlit.dataframe(my_data_row)
 
 # Section 12 Challegne #1
 # New section to display fruityvice api response
-fruit_choice = streamlit.text_input('What fruit would you like add','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like add','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-add_my_fruit = my_data_row.append(fruityvice_response)
-
-# Stores fruitvice fruit content in a variable
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# Display fruit content in a dataframe (aka table format)
-streamlit.dataframe(fruityvice_normalized)
-
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
