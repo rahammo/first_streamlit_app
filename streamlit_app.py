1d0c182a1478c35cd52f60c8cfb086eb70111375
rahammo <97483934+rahammo@users.noreply.github.com>
2022-08-15 15:10:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,8 +62,14 @@ if streamlit.button('Get Fruit Load List'):
 # Allow user to add a fruit to fruit load list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
         return "Thanks for adding " + new_fruit
+    
+add_my_fruit = streamlit.text_input('What fruit would you like add?')
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
     
 # Section 12 Challegne #1
     # New section to display fruityvice api response
